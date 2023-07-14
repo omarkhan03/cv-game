605fca250eb6bc9e746e2a325deef8adf370f690
omarkhan03 <omarahmadkhan2003@gmail.com>
2023-07-14 00:42:27 -0600

    added barriers

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 import pygame, sys
 from player import Player
 import cv2
+import obstacle
 
 class Game:
     def __init__(self):
         self.player_sprite = Player(((screen_width / 2), screen_height), screen_width, 5, video_width)
         self.player = pygame.sprite.GroupSingle(self.player_sprite)
 
+        # obstacle setup
+        self.shape = obstacle.shape
+        self.block_size = 6
+        self.blocks = pygame.sprite.Group()
+        self.obstacle_amount = 4
+        self.obstacle_x_positions = [num * (screen_width / self.obstacle_amount) for num in range(self.obstacle_amount)]
+        self.create_multiple_obstacles(*self.obstacle_x_positions, x_start=video_width + (screen_width/15), y_start=480)
+
+    def create_obstacle(self, x_start, y_start, offset_x):
+        for row_index, row in enumerate(self.shape):
+            for col_index, col in enumerate(row):
+                if col == 'x':
+                    x = x_start + col_index * self.block_size + offset_x
+                    y = y_start + row_index * self.block_size
+                    block = obstacle.Block(self.block_size, (241,79,80), x, y)
+                    self.blocks.add(block)
+
+    def create_multiple_obstacles(self, *offset, x_start, y_start):
+        for offset_x in offset:
+            self.create_obstacle(x_start, y_start, offset_x)
+
     def run(self):
         self.player.update()
         self.player.sprite.lasers.draw(screen)
         self.player.draw(screen)
+        self.blocks.draw(screen)
 
 
 if __name__ == '__main__':
