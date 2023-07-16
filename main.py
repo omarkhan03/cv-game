7acd80bb9954481420022aa61a25b28bf9bc42d0
omarkhan03 <omarahmadkhan2003@gmail.com>
2023-07-16 01:47:49 -0600

    added lives

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ class Game:
         self.player_sprite = Player(((screen_width / 2), screen_height), screen_width, 5, video_width)
         self.player = pygame.sprite.GroupSingle(self.player_sprite)
 
+        # health and score setup
+        self.lives = 3
+        self.live_surf = pygame.image.load('./Resources/player.png').convert_alpha()
+        self.live_x_start_pos = window_width - (self.live_surf.get_size()[0] * 2 + 20)
+
         # obstacle setup
         self.shape = obstacle.shape
         self.block_size = 6
@@ -103,9 +108,12 @@ class Game:
                 if pygame.sprite.spritecollide(laser, self.blocks, True):
                     laser.kill()
 
-                if pygame.sprite.spritecollide(laser, self.player, True):
+                if pygame.sprite.spritecollide(laser, self.player, False):
                     laser.kill()
-                    print('dead')
+                    self.lives -= 1
+                    if self.lives <= 0:
+                        pygame.quit()
+                        sys.exit()
 
         # aliens
         if self.aliens:
@@ -115,6 +123,11 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
+    def display_lives(self):
+        for live in range(self.lives - 1):
+            x = self.live_x_start_pos + (live * (self.live_surf.get_size()[0] + 10))
+            screen.blit(self.live_surf,(x,8))
+
     def run(self):
         self.player.update()
         self.aliens.update(self.alien_direction)
@@ -122,6 +135,8 @@ class Game:
         self.alien_lasers.update()
         self.extra_alien_timer()
         self.extra.update()
+        self.collision_checks()
+        self.display_lives()
 
         self.player.sprite.lasers.draw(screen)
         self.player.draw(screen)
@@ -130,7 +145,7 @@ class Game:
         self.alien_lasers.draw(screen)
         self.extra.draw(screen)
 
-        self.collision_checks()
+
 
 
 if __name__ == '__main__':
